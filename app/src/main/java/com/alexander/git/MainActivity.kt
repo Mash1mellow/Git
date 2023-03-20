927a4f94aa71ba4d611a8e6b0069aedb9a2aa158
alexander <alexander.rolind@gmail.com>
2023-03-20 10:10:21 +0800

    第二次 Commit

--- app/src/main/java/com/alexander/git/MainActivity.kt
+++ app/src/main/java/com/alexander/git/MainActivity.kt
@@ -2,10 +2,14 @@ package com.alexander.git
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var txv : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv = findViewById(R.id.txv)
+        txv.text="0"
     }
 }
